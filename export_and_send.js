3af1af887cf6dc9fa4f3b26be72dda83364b72b6
tanlocnguyen-lgtm <tanloc.nguyen@shopee.com>
2025-11-20 14:59:50 +0700

    Update export_and_send.js

--- export_and_send.js
+++ export_and_send.js
@@ -65,7 +65,7 @@ const MENTION_EMAILS = [
   "xuanbiu.nguyen@shopee.com",
   "hoangsang.tranvu@shopee.com",
   "luan.nguyen@shopee.com",
-  "quan.vominh@spxexpress.com"
+  "quan.vominh@spxexpress.com",
   "thien.nguyenvan@shopee.com"
   // additional mentions appended later (quang.huynh, anh.tranviet, dieu.buithuy, tanloc.nguyen)
 ];
@@ -74,7 +74,7 @@ const FOOTER_MENTIONS = [
   "quang.huynh@shopee.com",
   "anh.tranviet@shopee.com",
   "dieu.buithuy@shopee.com",
-  "tanloc.nguyen@shopee.com"
+  "tanloc.nguyen@shopee.com",
   "thien.nguyenvan@shopee.com"
 ];
 
